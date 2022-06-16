77508431ed1bd51c492b1bd2c14a37ac394fa8f9
Hadi Raza <hadiraza.edu@gmail.com>
2022-06-17 00:06:13 +0500

    Add files via upload

---
+++ C++ (1st Program).cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+main(){
+	using namespace std;
+	cout<<"Integer size in bytes = " <<sizeof (int)<<endl;
+	cout<<"Float size in bytes = " <<sizeof (float)<<endl;
+	cout<<"Double size in bytes = " <<sizeof (double)<<endl;
+	cout<<"String size in bytes = " <<sizeof (string)<<endl;
+	cout<<"Character size in bytes = " <<sizeof (char)<<endl;
+	cout<<"Bool size in bytes = " <<sizeof (bool)<<endl;
+	cout<<""<<endl;
+	
+}
+
